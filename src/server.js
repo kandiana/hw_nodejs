e9f47864eb2b1fdfd7e6807c8899583fe6286300
kandiana <dianka95@gmail.com>
2021-10-01 19:53:28 +0300

    added a way to upload files

--- src/server.js
+++ src/server.js
@@ -1,12 +1,34 @@
 const express = require('express')
 const { PORT } = require('./config')
 
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, __dirname + '/imgs/')
+	},
+	filename: function (req, file, cb) {
+		cb(null, file.originalname) // modified here  or user file.mimetype
+	},
+})
+
+const upload = multer({ storage: storage })
+
 const app = express()
 
 app.use(express.json())
 
 app.get('/ping', (req, res) => {
-	res.send('pong')
+	res.json({ ping: 'pong' })
+})
+
+app.post('/upload', upload.single('image'), (req, res) => {
+	console.log(req.file, req.body)
+	res.send('ok')
+})
+
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/index.html')
 })
 
 app.listen(PORT, () => {
